d322edc75dca3a6deeeb26a8695224fd9e91800f
HiGuy Smith <higuymb@gmail.com>
2016-07-04 15:38:24 -0400

    Separate this stuff

--- scripts/gl.js
+++ scripts/gl.js
@@ -9,6 +9,8 @@ var cameraRotation = vec2.create();
 var time = 0;
 var lastTimestamp = null;
 
+var physics = false;
+
 function initGL() {
 	canvas = document.getElementById("screen");
 	/** @type {WebGLRenderingContext} gl */
@@ -25,7 +27,9 @@ function initGL() {
 	initBuffers();
 	initTextures();
 
-	initPhysics();
+	if (physics) {
+		initPhysics();
+	}
 
 	//Then start running!
 	window.requestAnimFrame(render);
@@ -48,8 +52,9 @@ function initBuffers() {
 	vbo.addAttribute("in_tangent", 3, gl.FLOAT, false, 8);
 	vbo.addAttribute("in_bitangent", 3, gl.FLOAT, false, 11);
 
-	sphere = new Sphere(0.3);
-	sphere.generate();
+	if (physics) {
+		initPhysicsBuffers();
+	}
 }
 
 function initTextures() {
@@ -66,75 +71,6 @@ function initTextures() {
 	});
 }
 
-function initPhysics() {
-	var config     = new Ammo.btDefaultCollisionConfiguration();
-	dispatcher     = new Ammo.btCollisionDispatcher(config);
-	var broadphase = new Ammo.btDbvtBroadphase();
-	var solver     = new Ammo.btSequentialImpulseConstraintSolver();
-	world          = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, config);
-	world.setGravity(new Ammo.btVector3(0, 0, -20));
-
-	{
-		var state = new Ammo.btDefaultMotionState();
-		var shape = new Ammo.btSphereShape(0.3);
-
-		var fallInertia = new Ammo.btVector3(0, 0, 0);
-		shape.calculateLocalInertia(1.0, fallInertia);
-		shape.setMargin(0.01);
-
-		//Update position
-		var transform = new Ammo.btTransform();
-		transform.setIdentity();
-
-		state.setWorldTransform(transform);
-
-		//Construction info
-		var info = new Ammo.btRigidBodyConstructionInfo(1, state, shape, fallInertia);
-		info.set_m_restitution(0.5); // 0.5 * 0.7
-		info.set_m_friction(1.0);
-		info.set_m_rollingFriction(0.3);
-
-		//Create the actor and add it to the scene
-		physSphere = new Ammo.btRigidBody(info);
-		physSphere.setActivationState(4);
-		physSphere.setCcdMotionThreshold(1e-3);
-		physSphere.setCcdSweptSphereRadius(0.3 / 10.0);
-		physSphere.setRollingFriction(3.0);
-		physSphere.setContactProcessingThreshold(0.0);
-
-		world.addRigidBody(physSphere);
-	}
-	{
-		var state = new Ammo.btDefaultMotionState();
-		var mesh  = new Ammo.btTriangleMesh();
-
-		for (var i = 0; i < model.faces.length; i += 3 * 14) {
-			var v0 = new Ammo.btVector3(model.faces[i + (14 * 0) + 0], model.faces[i + (14 * 0) + 1], model.faces[i + (14 * 0) + 2]);
-			var v1 = new Ammo.btVector3(model.faces[i + (14 * 1) + 0], model.faces[i + (14 * 1) + 1], model.faces[i + (14 * 1) + 2]);
-			var v2 = new Ammo.btVector3(model.faces[i + (14 * 2) + 0], model.faces[i + (14 * 2) + 1], model.faces[i + (14 * 2) + 2]);
-
-			mesh.addTriangle(v0, v1, v2);
-		}
-
-		var shape = new Ammo.btBvhTriangleMeshShape(mesh, true, true);
-		shape.setMargin(0.01);
-
-		var transform = new Ammo.btTransform();
-		transform.setIdentity();
-
-		state.setWorldTransform(transform);
-
-		var inertia = new Ammo.btVector3(0, 0, 0);
-		var info = new Ammo.btRigidBodyConstructionInfo(0, state, shape, inertia);
-
-		var actor = new Ammo.btRigidBody(info);
-		actor.setRestitution(1.0);
-		actor.setFriction(1.0);
-
-		world.addRigidBody(actor);
-	}
-}
-
 function render(timestamp) {
 	//Get the delta time since the last frame
 	var delta = 0;
@@ -145,15 +81,11 @@ function render(timestamp) {
 	}
 	lastTimestamp = timestamp;
 
-	world.stepSimulation(delta, 1);
-	var transform = new Ammo.btTransform();
-	physSphere.getMotionState().getWorldTransform(transform);
-
-	var origin = transform.getOrigin();
-	var rotation = transform.getRotation();
-
-	var marblePos = [origin.x(), origin.y(), origin.z()];
-	var marbleRot = [rotation.x(), rotation.y(), rotation.z(), rotation.w()];
+	if (physics) {
+		updatePhysics(delta);
+	} else {
+		
+	}
 
 	//Check if the window updated its size. If so, we need to update the canvas and viewport to match.
 	var density = 1;
@@ -164,11 +96,9 @@ function render(timestamp) {
 		gl.viewport(0, 0, canvas.clientWidth * density, canvas.clientHeight * density);
 	}
 
-	sphere.updateMovement(delta / 1000.0);
-
 	//Get the inverse camera position because we move the world instead of the camera
 	var inverseCamera = vec3.create();
-	vec3.scale(inverseCamera, marblePos, -1);
+	vec3.scale(inverseCamera, cameraPosition, -1);
 
 	//Three fundamental matrices
 	var projectionMat = mat4.create();
@@ -244,10 +174,9 @@ function render(timestamp) {
 		shader.deactivate();
 	}
 
-	modelMat = mat4.create();
-	mat4.fromRotationTranslation(modelMat, marbleRot, marblePos);
-
-	sphere.render(projectionMat, viewMat, modelMat);
+	if (physics) {
+		physicsRender();
+	}
 
 	//Tell the browser to get us the next frame
 	window.requestAnimFrame(render);
