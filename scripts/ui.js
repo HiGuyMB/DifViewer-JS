85d23069975309cffd526aa87b4a663f82aa3638
HiGuy Smith <higuymb@gmail.com>
2016-07-13 21:33:08 -0400

    Infer texture group from the dif

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,69 @@
+var obviousMats = [
+	{album: "mbg", textures:[
+		"grid_cool",
+		"grid_cool2",
+		"grid_cool3",
+		"grid_cool4",
+		"grid_neutral",
+		"grid_neutral1",
+		"grid_neutral2",
+		"grid_neutral3",
+		"grid_neutral4",
+		"grid_warm",
+		"grid_warm1",
+		"grid_warm2",
+		"grid_warm3",
+		"grid_warm4.jpg",
+		"pattern_cool2"
+	]},
+	{album: "mbp", textures:[
+		"mbp_hot2",
+		"mbp_hot3",
+		"mbp_hot4",
+		"mbp_hot5",
+		"mbp_hot6",
+		"mbp_hot7",
+		"mbp_neon1",
+		"mbp_neon2",
+		"mbp_neon3",
+		"mbu_grid_blue1",
+		"mbu_grid_cool1",
+		"mbu_grid_green1",
+		"mbu_grid_green2",
+		"mbu_grid_hot1",
+		"mbu_grid_neutral1",
+		"mbu_grid_warm5",
+		"mbu_neutral",
+		"mbu_neutral2",
+		"mbu_neutral3",
+		"mbu_neutral4",
+		"mbu_neutral5",
+		"mbu_pattern_cool2"
+	]},
+	{album: "mbu", textures:[
+		"plate_1",
+		"tile_advanced_blue_shadow",
+		"tile_advanced_blue",
+		"tile_advanced_green_shadow",
+		"tile_advanced_green",
+		"tile_advanced_shadow",
+		"tile_advanced",
+		"tile_beginner_blue_shadow",
+		"tile_beginner_blue",
+		"tile_beginner_red_shadow",
+		"tile_beginner_red",
+		"tile_beginner_shadow",
+		"tile_beginner",
+		"tile_intermediate_green_shadow",
+		"tile_intermediate_green",
+		"tile_intermediate_red_shadow",
+		"tile_intermediate_red",
+		"tile_intermediate_shadow",
+		"tile_intermediate",
+		"tile_underside"
+	]}
+];
+
 document.getElementById("picker").addEventListener("change", function (e) {
 	var files = e.target.files;
 	for (var i = 0; i < files.length; i ++) {
@@ -12,6 +78,22 @@ document.getElementById("picker").addEventListener("change", function (e) {
 				var dif = new DIF(rawBytes);
 				var model = dif.getModel(0);
 				models.push(new InteriorModel(model));
+
+				var textures = dif.interiors[0].material;
+				obviousMats.forEach(function(group) {
+					var album = group.album;
+					group.textures.forEach(function(texture) {
+						if (textures.indexOf(texture) != -1) {
+							var picker = document.getElementById("texChoice");
+							for (var index = 0; index < picker.children.length; index ++) {
+								var child = picker.children[index];
+								if (child.value == album)
+									picker.selectedIndex = index;
+							}
+							texChoice = album;
+						}
+					});
+				});
 
 				var infoTable = document.getElementById("difInfo");
 
@@ -105,7 +187,7 @@ document.getElementById("replayPicker").addEventListener("change", function(e) {
 
 				var infoDiv = document.getElementById("replayInfo");
 				var replay;
-				
+
 				try {
 					replay = JSON.parse(raw);
 				} catch (err) {
